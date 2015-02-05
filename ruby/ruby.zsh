6eaac71f98c4995ff139979a9b57365e356c198b
Tony Lotts <tonylotts@users.noreply.github.com>
2015-02-05 17:10:29 -0500

    Use chruby if installed

--- ruby/ruby.zsh
+++ ruby/ruby.zsh
@@ -1,2 +1,5 @@
-source /usr/local/share/chruby/chruby.sh
-source /usr/local/share/chruby/auto.sh
+# Use chruby if installed
+if [ -d "/usr/local/share/chruby" ]; then 
+  source /usr/local/share/chruby/chruby.sh
+  source /usr/local/share/chruby/auto.sh
+fi
